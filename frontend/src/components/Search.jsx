941a423fd030b6fdc8039626bea0dad79c45d18a
jeff <>
2022-10-03 18:28:01 -0400

    commented out geocode

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -114,25 +114,25 @@ const Search = () => {
     }
   };
 
-  const GeoCode = () => {
-    const location = userAddress;
-    axios
-      .get("https://maps.googleapis.com/maps/api/geocode/json", {
-        params: {
-          address: location,
-          key: googleKey,
-        },
-      })
-      .then((response) => {
-        setLat(response?.data?.results[0].geometry.location.lat);
-        setLong(response?.data?.results[0].geometry.location.lng);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
+  // const GeoCode = () => {
+  //   const location = userAddress;
+  //   axios
+  //     .get("https://maps.googleapis.com/maps/api/geocode/json", {
+  //       params: {
+  //         address: location,
+  //         key: googleKey,
+  //       },
+  //     })
+  //     .then((response) => {
+  //       setLat(response?.data?.results[0].geometry.location.lat);
+  //       setLong(response?.data?.results[0].geometry.location.lng);
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  // };
 
-  GeoCode();
+  // GeoCode();
 
   function Map() {
     const [activeMarker, setActiveMarker] = useState("");
